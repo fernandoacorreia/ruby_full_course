6fca225d5d14e33cc6587603ccce627a9cbeac9d
Fernando Correia <fernandoacorreia@gmail.com>
2022-03-26 10:31:30 -0700

    Guessing 2

--- guessing.rb
+++ guessing.rb
@@ -2,10 +2,22 @@
 
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
-while guess != secret_word
-  puts "Enter your guess: "
-  guess = gets.chomp()
+while guess != secret_word and !out_of_guesses
+  if guess_count < guess_limit
+    puts "Enter your guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else
+    out_of_guesses = true
+  end
 end
 
-puts "You guessed right!"
+if out_of_guesses
+  puts "You're out of guesses."
+else
+  puts "You guessed right!"
+end
